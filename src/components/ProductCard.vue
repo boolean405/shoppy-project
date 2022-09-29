693b0ac13bf54423fd395b4ff41bb3f2ef739a8d
Bo Bo Aung <maungbo404@gmail.com>
2022-09-29 19:46:40 +0630

    update components

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="card p-3 mt-5 mb-3 ">
+        <img :src="product.image" class="card-img-top " height="300">
+        <div class="card-body">
+            <h6 class="card-title">{{product.title.substr(0,20)}}</h6>
+            <p class="card-text">$ {{product.price}}</p>
+            <a href="#" class="btn btn-outline-dark">Add to cart</a>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "ProductCard",
+    props: {
+        product: Object,
+    }
+} 
+</script>
--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -19,14 +19,7 @@
         <div class="row">
             <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12" v-for="(product,index) in products" :key="index">
                 <div>
-                    <div class="card p-3 mt-5 mb-3 ">
-                        <img :src="product.image" class="card-img-top " height="300">
-                        <div class="card-body">
-                            <h6 class="card-title">{{product.title.substr(0,20)}}</h6>
-                            <p class="card-text">$ {{product.price}}</p>
-                            <a href="#" class="btn btn-outline-dark">Add to cart</a>
-                        </div>
-                    </div>
+                  <ProductCard :product="product"></ProductCard>
                 </div>
             </div>
         </div>
@@ -37,24 +30,24 @@
 </template>
 
 <script>
-
+import ProductCard from '@/components/ProductCard.vue'
 const axios = require('axios')
 
 export default {
-    name: 'ProductsView',
-
+    name: "ProductsView",
     data() {
         return {
             products: [],
-        }
+        };
     },
     mounted() {
-        axios.get('https://fakestoreapi.com/products')
+        axios.get("https://fakestoreapi.com/products")
             .then(response => {
-                this.products = response.data
-                // console.log(this.products[1].id);
-            })
-    }
+            this.products = response.data;
+            // console.log(this.products[1].id);
+        });
+    },
+    components: { ProductCard }
 }
 </script>
 <style>
