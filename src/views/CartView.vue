d5df54892e7327fee34510614d5611f6a22dd2d6
Bo Bo Aung <maungbo404@gmail.com>
2022-10-05 20:46:38 +0630

    add cart view

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,61 +1,72 @@
 <template>
     <HeaderText title="All Carts" text="Start order now!"></HeaderText>
-    <div class="container my-5 ">
-        <div class="row ">
-            <div class="col-12" v-if="carts">
-                <!-- <button class="btn btn-primary" type="button">Button</button> -->
-                <button class="btn btn-outline-dark float-end" type="button" @click="removeAllCarts()">Remove Items from
-                    cart</button>
+
+    <div v-if="carts">
+        <div class="container my-5 ">
+            <div class="row ">
+                <div class="col-12">
+                    <!-- <button class="btn btn-primary" type="button">Button</button> -->
+                    <button class="btn btn-outline-dark float-end" type="button" @click="removeAllCarts()">Remove Items
+                        from cart</button>
+                </div>
             </div>
         </div>
-    </div>
-    <!-- cart -->
-    <div class="container" v-if="carts">
-        <div class="row" v-for="(cart,index) in carts" :key="index">
-            <div class="col">
-                <div class="card mb-5">
-                    <div class="row g-0">
-                        <div class="col-md-4">
-                            <img :src="cart.image" class="img-fluid rounded mx-auto d-block w-25 my-3">
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h5 class="card-title fs-3">{{cart.title}}</h5>
-                                <p class="card-text fs-4">$ {{cart.price}}</p>
-                                <div>
-                                    <button type="button" class="btn btn-outline-dark btn-sm"
-                                        @click="cartQtyDecr(cart)">
-                                        <i class="fa-solid fa-minus"></i></button>
+        <!-- cart -->
+        <div class="container">
+            <div class="row" v-for="(cart,index) in carts" :key="index">
+                <div class="col">
+                    <div class="card mb-5">
+                        <div class="row g-0">
+                            <div class="col-md-4">
+                                <img :src="cart.image" class="img-fluid rounded mx-auto d-block w-25 my-3">
+                            </div>
+                            <div class="col-md-8">
+                                <div class="card-body">
+                                    <h5 class="card-title fs-3">{{cart.title}}</h5>
+                                    <p class="card-text fs-4">$ {{cart.price}}</p>
+                                    <div>
+                                        <button type="button" class="btn btn-outline-dark btn-sm"
+                                            @click="cartQtyDecr(cart)">
+                                            <i class="fa-solid fa-minus"></i></button>
 
-                                    <span class="mx-5 fs-5">{{cart.qty}}
-                                        <span v-if="cart.qty >1">Items</span>
-                                        <span v-else>Item</span>
-                                    </span>
+                                        <span class="mx-5 fs-5">{{cart.qty}}
+                                            <span v-if="cart.qty >1">Items</span>
+                                            <span v-else>Item</span>
+                                        </span>
 
-                                    <button type="button" class="btn btn-outline-dark btn-sm" @click="cart.qty++">
-                                        <i class="fa-solid fa-plus"></i></button>
+                                        <button type="button" class="btn btn-outline-dark btn-sm" @click="cart.qty++">
+                                            <i class="fa-solid fa-plus"></i></button>
 
 
+                                    </div>
+                                    <p class="my-3">Total : $ {{cart.price * cart.qty}}</p>
                                 </div>
-                                <p class="my-3">Total : $ {{cart.price * cart.qty}}</p>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    <!-- cart -->
+        <!-- cart -->
 
-    <!-- check out -->
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <button type="button" class="btn btn-outline-dark px-5 float-end">Check Out</button>
+
+        <!-- check out -->
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <button type="button" class="btn btn-outline-dark px-5 float-end">Check Out</button>
+                </div>
             </div>
         </div>
+        <!-- check out -->
     </div>
-    <!-- check out -->
+
+    <div class="v-else">
+        <h2 class="text-center">You have no added items. Go to shop now!</h2>
+        <button type="button" class="btn btn-outline-dark px-5 mx-auto d-block my-3">Shop Now</button>
+        <img src="../assets/images/cart/girl_no_cart.jpg" class="img-fluid w-25 rounded mx-auto d-block">
+
+    </div>
 </template>
 
 <script>
